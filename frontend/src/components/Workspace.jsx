3e3ab60db7b6d5d880ea65ec9d0c6b0c150c3ed4
bhupeshpatil-sudo <bhupeshpatil230306@gmail.com>
2026-07-18 16:59:10 +0530

    Frontend connected to backend

--- frontend/src/components/Workspace.jsx
+++ frontend/src/components/Workspace.jsx
@@ -30,7 +30,7 @@ function Workspace({ selectedTool }) {
 
     try {
       const res = await axios.post(
-        "http://127.0.0.1:8000/generate",
+        "https://devpilot-ai-4gi4.onrender.com/generate",
         {
           tool: selectedTool,
           text: prompt,
